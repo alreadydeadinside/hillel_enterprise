59c51fee86bcc1bdd3ff1b58da8aa695c84b78ec
Bohdan Vakaliuk <54852515+BoleanSwift@users.noreply.github.com>
2021-03-05 18:22:20 +0200

    [HW1] Launch Spring app and add @Slf4j annotation, successfully checked, if app working by a console command ./mvnw spring-boot:run

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@Slf4j
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(DemoApplication.class);
+		log.info("Test log");
+	}
+
+}
